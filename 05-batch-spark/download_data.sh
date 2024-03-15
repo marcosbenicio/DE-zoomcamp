93e5a4dc16fb06d8aae150c8ae9e794757df5997
marcosbenicio <marcosbenicio0102@gmail.com>
2024-03-15 20:41:35 +0000

    Explaining how prepare data spark

--- 05-batch-spark/download_data.sh
+++ 05-batch-spark/download_data.sh
@@ -1,21 +1,26 @@
+# Shell exit immediately if a command exits with a error
 set -e
 
-TAXI_TYPE=$1 # "yellow"
-YEAR=$2 # 2020
-
-URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
+# Args to pass when running the script
+# ./download_data.sh {1} {2}
+TAXI_TYPE=$1  # "yellow"/"green"
+YEAR=$2       # 2022/2023
+URL_PREFIX="https://d37ci6vzurychx.cloudfront.net/trip-data"
 
 for MONTH in {1..12}; do
+  # Format integers to have 2 digits
   FMONTH=`printf "%02d" ${MONTH}`
-
-  URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
+  URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.parquet"
 
   LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
-  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
+  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.parquet"
   LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
 
   echo "downloading ${URL} to ${LOCAL_PATH}"
+  # Create the specified directory and any parent directories as needed
   mkdir -p ${LOCAL_PREFIX}
+
+  # Download from url
   wget ${URL} -O ${LOCAL_PATH}
 
 done
